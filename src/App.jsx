80124f94253fd8371a1d4d35147799f0b21dfa26
ZenDenStays <info@zendenhomes.co.in>
2025-04-21 14:55:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Zen Den</h1>
+      <p>Your soulful stay platform is ready to go live ✨</p>
+    </div>
+  );
+}
